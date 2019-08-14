0a6c10a2ed738122b80f3d5acbc7e808ee485794
mttsai <a17653aw96801@gmail.com>
2019-08-14 07:00:51 -0700

    map wrap around

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,11 @@ PIXI.utils.sayHello(type)
 //Create a Pixi Application
 let app = new PIXI.Application({width: 1024, height: 768});
 
-var bg;
+let bg;
+let ripples = [];
+app.ticker.add(() => {
+    ripples.forEach(ripple => ripple.update());
+});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
@@ -37,113 +41,121 @@ class Ripple {
   }
 }
 
-var player;
+/* refraction filter */
+/*********************
+ * var displace = PIXI.Sprite.fromImage("images/ink.png");
+ * var filter = new PIXI.filters.DisplacementFilter(displace);
+ * filter.autoFit = true;
+ * displace.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+ * displace.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+ * displace.scale.y = 1;
+ * displace.scale.x = 1;
+ *********************/
+
+let show_ripple = function (x, y) {
+    console.log('shot ripple');
+    ripples.push(new Ripple(x, y, bg));
+    bg.filters = [...ripples.map(f => f.filter)];
+};
+
+let shot = function(_b) {
+    _b.set_radius(0);
+    app.stage.addChild(_b.image);
+
+    _b.elapsed = 300;
+
+    _b.ticker = new PIXI.ticker.Ticker();
+    _b.ticker.autoStart = false;
+
+    _b.ticker.add(function(delta) {
+        // move
+        _b.image.x += (_b.elapsed / 100 < 1) ? (1) : (_b.elapsed / 100);
+        // enlarge
+        _b.set_radius((300 - _b.elapsed) / 10)
+
+        _b.elapsed -= delta;
+        if (_b.elapsed < 0) {
+            _b.ticker.stop()
+
+            // clear bullet first, then explosion
+            _b.explosion(function() {
+                app.stage.removeChild(_b.image);
+                delete _b;
+            })
+            console.log('remove Bullet');
+        }
+
+    });
+
+    _b.ticker.start();
+};
+
+let shot_bullet = function(_color) {
+    console.log('shot');
+
+    show_ripple(player.image.x, player.image.y)
+
+    var _b = new sprite.Bullet(player.image.x,
+                               player.image.y,
+                               _color);
+
+    shot(_b);
+}
+let shot_rainbow_bullet = function() {
+    console.log('shot rainbow');
+
+    var _b = new sprite.RainbowBullet(player.image.x,
+                                      player.image.y);
+
+    shot(_b);
+}
+
+var player, enemy;
+var mouseX = -1;
+var mouseY = -1;
+
 function setup() {
     /* Background */
     bg = new PIXI.Container();
     app.stage.addChild(bg);
 
-    land = new PIXI.Sprite(PIXI.loader.resources["images/land.jpg"].texture);
-    app.renderer.resize(land.width, land.height);
+    land = [new PIXI.Sprite(PIXI.loader.resources["images/land.jpg"].texture),
+            new PIXI.Sprite(PIXI.loader.resources["images/land.jpg"].texture)]
+    app.renderer.resize(land[0].width, land[0].height);
+    land[1].scale.x = -1
+    land[1].anchor.x = 1
+    land[1].x = land[0].width
+
+    bg.addChild(land[0]);
+    bg.addChild(land[1]);
+
+    /* Background wrap around */
+    app.ticker.add(function() {
+        for (var i = 0; i < 2; i++) {
+            land[i].x -= 1;
+            if (land[i].x + land[i].width == 0) {
+                land[i].x = land[i].width;
+            }
+        }
+    });
 
 
-    /* refraction filter */
-    var displace = PIXI.Sprite.fromImage("images/ink.png");
-    var filter = new PIXI.filters.DisplacementFilter(displace);
-    filter.autoFit = true;
-    displace.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-    displace.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-    displace.scale.y = 1;
-    displace.scale.x = 1;
-    bg.addChild(displace);
-
-    bg.addChild(land);
-
-	let ripples = [];
-	app.ticker.add(() => {
-		ripples.forEach(ripple => ripple.update());
-	});
-
-	let show_ripple = function (x, y) {
-        console.log('shot ripple');
-	    ripples.push(new Ripple(x, y, bg));
-	    bg.filters = [...ripples.map(f => f.filter)];
-	};
-
-    /* Play */
-    player = new sprite.Player();
+    /* Player */
+    player = new sprite.Player(0xff00ff);
 
     app.stage.addChild(player.image);
 
     console.log("PIXI.loader.load()");
 
+    /* Player along with player */
     app.ticker.add(function(delta) {
-        var mouseX = app.renderer.plugins.interaction.mouse.global.x;
-        var mouseY = app.renderer.plugins.interaction.mouse.global.y;
-
         if (mouseX < 0 || mouseY < 0) {
             mouseX = mouseY = 384;
         }
 
-        var targetX = mouseX - player.image.width / 2;
-        var targetY = mouseY - player.image.height / 2;
-
         player.image.x += (mouseX - player.image.x) * 0.0625;
         player.image.y += (mouseY - player.image.y) * 0.0625;
     });
-
-    var shot = function(_b) {
-        _b.set_radius(0);
-        app.stage.addChild(_b.image);
-
-        _b.elapsed = 300;
-
-        _b.ticker = new PIXI.ticker.Ticker();
-        _b.ticker.autoStart = false;
-
-        _b.ticker.add(function(delta) {
-            // move
-            _b.image.x += (_b.elapsed / 100 < 1) ? (1) : (_b.elapsed / 100);
-            // enlarge
-            _b.set_radius((300 - _b.elapsed) / 10)
-
-            _b.elapsed -= delta;
-            if (_b.elapsed < 0) {
-                _b.ticker.stop()
-
-                // clear bullet first, then explosion
-                _b.explosion(function() {
-                    app.stage.removeChild(_b.image);
-                    delete _b;
-                })
-                console.log('remove Bullet');
-            }
-
-        });
-
-        _b.ticker.start();
-    };
-
-    var shot_bullet = function(_color) {
-        console.log('shot');
-
-		show_ripple(player.image.x, player.image.y)
-
-        var _b = new sprite.Bullet(player.image.x,
-                                   player.image.y,
-                                   _color);
-
-        shot(_b);
-    }
-    var shot_rainbow_bullet = function(ev) {
-        console.log('shot rainbow');
-
-        var _b = new sprite.RainbowBullet(player.image.x,
-                                          player.image.y);
-
-        shot(_b);
-    }
-
 
     player.image.interactive = true;
 
@@ -169,7 +181,21 @@ function setup() {
 
     app.stage.interactive = true;
     // app.stage.click = _ => shot_bullet(0xffffff);
-    app.stage.on('pointerdown', _ => shot_bullet())
+    function mouse_pos_update(ev) {
+        var {x, y} = ev.data.getLocalPosition(bg);
+
+        if (x >= 0 && x < land[0].width &&
+            y >= 0 && y < land[0].height) {
+            mouseX = x; mouseY = y;
+        }
+    }
+    app.stage.on('pointerdown', function(ev) {
+        mouse_pos_update(ev);
+        shot_bullet();
+    });
+    app.stage.on('pointermove', function(ev) {
+        mouse_pos_update(ev);
+    });
 
     // window.onkeyup = function(e) {}
     window.onkeydown = function(e) {
@@ -182,6 +208,13 @@ function setup() {
             shot_rainbow_bullet()
         }
     }
+
+    /* Enemy */
+    enemy = new sprite.Player(0x0000ff, true);
+    app.stage.addChild(enemy.image);
+    enemy.image.x = land[0].width - 100;
+    enemy.image.y = land[0].height / 2;
+    // enemy.filters = [new PIXI.filters.GlowFilter(15, 2, 1, 0xFF0000, 0.5)];
 }
 
 function enlarge() {
